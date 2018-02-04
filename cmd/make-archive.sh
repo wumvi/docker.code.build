e9752ab316446ebdf97d0f66253b464ac2346f8c
wumvi <vk@wumvi.com>
2018-02-04 21:19:52 +0300

    change

--- cmd/make-archive.sh
+++ cmd/make-archive.sh
@@ -11,13 +11,3 @@ for i in `find ./* -type f -name '*.svg'`; do echo $i; gzip -c -9 $i > $i.gz; to
 for i in `find ./* -type f -name '*.js'`; do echo $i; /usr/sbin/bro --input ${i} --force --output ${i}.br; done;
 for i in `find ./* -type f -name '*.css'`; do echo $i; /usr/sbin/bro --input ${i} --force --output ${i}.br; done;
 for i in `find ./* -type f -name '*.svg'`; do echo $i; /usr/sbin/bro --input ${i} --force --output ${i}.br; done;
-
-### Удаление Gzip
-for i in `find ./public/res/* -type f -name '*.js.gz'`; do echo $i; rm $i; done;
-for i in `find ./public/res/* -type f -name '*.css.gz'`; do echo $i; rm $i; done;
-for i in `find ./public/res/* -type f -name '*.svg.gz'`; do echo $i; rm $i; done;
-
-##### Удаление Brotli
-for i in `find ./public/res/* -type f -name '*.js.br'`; do echo $i; rm $i; done;
-for i in `find ./public/res/* -type f -name '*.css.br'`; do echo $i; rm $i; done;
-for i in `find ./public/res/* -type f -name '*.svg.br'`; do echo $i; rm $i; done;
