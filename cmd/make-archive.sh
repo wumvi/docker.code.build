f8d38586b63c46d422b66728c2f5d2f33ec6792f
wumvi <vk@wumvi.com>
2018-02-04 22:51:01 +0300

    fix brotli error

--- cmd/make-archive.sh
+++ cmd/make-archive.sh
@@ -3,11 +3,7 @@
 cd $1
 
 ##### Сжатие Gzip
-for i in `find ./* -type f -name '*.js'`; do echo $i; gzip -c -9 $i > $i.gz; touch -r $i $i.gz; done;
-for i in `find ./* -type f -name '*.css'`; do echo $i; gzip -c -9 $i > $i.gz; touch -r $i $i.gz; done;
-for i in `find ./* -type f -name '*.svg'`; do echo $i; gzip -c -9 $i > $i.gz; touch -r $i $i.gz; done;
+for i in `find ./* -type f \( -iname \*.svg -o -iname \*.js -o -iname \*.css \)`; do echo $i; gzip -c -9 $i > $i.gz; touch -r $i $i.gz; done;
 
 ##### Сжатие Brotli
-for i in `find ./* -type f -name '*.js'`; do echo $i; /usr/sbin/bro --input ${i} --force --output ${i}.br; done;
-for i in `find ./* -type f -name '*.css'`; do echo $i; /usr/sbin/bro --input ${i} --force --output ${i}.br; done;
-for i in `find ./* -type f -name '*.svg'`; do echo $i; /usr/sbin/bro --input ${i} --force --output ${i}.br; done;
+for i in `find ./* -type f \( -iname \*.svg -o -iname \*.js -o -iname \*.css \)`; do echo $i; brotli --force --output=${i}.br --quality=11 ${i}; done;
