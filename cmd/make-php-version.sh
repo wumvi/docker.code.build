0be2b7751bee438daf138e22057798da094bf3a1
wumvi <vk@wumvi.com>
2018-02-08 01:46:49 +0300

    Update make-php-version.sh

--- cmd/make-php-version.sh
+++ cmd/make-php-version.sh
@@ -2,5 +2,5 @@
 
 TIMESTAMP=`date +%s`
 DATE=`date`
-TEXT="<?php namespace Site\BuildInfo; class Version { public const REF = '$CI_COMMIT_REF_NAME';\npublic const SHA = '$CI_COMMIT_SHA';\npublic const TIMESTAMP = $TIMESTAMP;\npublic const DATE = '$DATE';\n}\n"
+TEXT="<?php namespace Site; class BuildInfo { public const REF = '$CI_COMMIT_REF_NAME';\npublic const SHA = '$CI_COMMIT_SHA';\npublic const TIMESTAMP = $TIMESTAMP;\npublic const DATE = '$DATE';\n}\n"
 echo -e $TEXT
